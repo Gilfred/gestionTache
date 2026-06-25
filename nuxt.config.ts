4d5f0bcd741726deda48b6a3650117dac79650a7
Gilfred Dgb <gilfredmawulomdgb@gmail.com>
2026-06-25 14:25:50 +0100

    intégration de clarity

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,5 +11,24 @@ export default defineNuxtConfig({
     configPath: 'tailwind.config.js',
     exposeConfig: true,
     viewer: true,
+  },
+
+  app: {
+    head: {
+      script: [
+        {
+          type: "text/javascript",
+          innerHTML: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;
+                t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];
+                y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "xckd2pk0ci");
+          `
+        }
+      ]
+    }
   }
 })
